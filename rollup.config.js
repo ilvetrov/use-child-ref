389d457c6e81338089ec208cfa6615b6af835bd3
ilvetrov <ilya.ilvetrov@gmail.com>
2022-12-24 21:41:25 +0400

    CJS and ESM

--- rollup.config.js
+++ rollup.config.js
@@ -7,12 +7,24 @@ export default [
     input: 'src/useChildRef.tsx',
     output: [
       {
-        file: pkg.exports['.'],
+        file: pkg.exports['.'].import,
         format: 'es',
         exports: 'named',
       },
     ],
     plugins: [typescript({ include: ['src/useChildRef.tsx'] }), cleanup()],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'src/useChildRef.tsx',
+    output: [
+      {
+        file: pkg.exports['.'].require,
+        format: 'cjs',
+        exports: 'default',
+      },
+    ],
+    plugins: [typescript({ include: ['src/useChildRef.tsx'], declaration: false }), cleanup()],
     external: ['react', 'react-dom'],
   },
 ]
